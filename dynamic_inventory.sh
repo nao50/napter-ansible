73fc020bb38305ed90ba8383e78ad9e2f1792f1d
naoyamaguchi <naoyamaguchi0128@gmail.com>
2019-11-28 18:36:36 +0900

    fix var type

--- dynamic_inventory.sh
+++ dynamic_inventory.sh
@@ -10,16 +10,16 @@ len=`echo ${subscriberList} | jq length`
 for i in `seq 0 $(expr ${len} - 1)`
 do
     subscriber=`echo $subscriberList | jq -r .[${i}]`
-    existe_port_mapping=`soracom port-mappings get --imsi $subscriber --profile $SORACOM_CLI_PROFILE | jq .`
+    existing_port_mapping=`soracom port-mappings get --imsi $subscriber --profile $SORACOM_CLI_PROFILE | jq .`
 
-    len=`echo ${existe_port_mapping} | jq length`;
+    len=`echo ${existing_port_mapping} | jq length`;
     if [ $len -eq 0 ]; then
         new_port_mapping=`soracom port-mappings create --body '{"destination": {"imsi": "'$subscriber'", "port": 22}}' --profile $SORACOM_CLI_PROFILE | jq .`
         arr+=`echo $new_port_mapping | jq .`
         continue
     fi
 
-    arr+=`echo $existe_port_mapping | jq .[]`
+    arr+=`echo $existing_port_mapping | jq .[]`
 
 done
 
