39eccb3f76492688981b4557313f05a89f5116a9
naoyamaguchi <naoyamaguchi0128@gmail.com>
2019-11-28 16:36:40 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # napter-ansible
 soracom napter with ansible dynamic inventory example
+
+# prerequire
+ansible 
+soracom cli
+
+# configure
+You can set variables that are appropriate for your environment by editing `dynamic_inventory.sh`.
+
+SORACOM_CLI_PROFILE : set your SORACOM CLI profile name
+TARGET_TAG_KEY : Set the `tag key` set in the SIM you want to set with napter.
+TARGET_TAG_VALUE : Set the `tag value` set in the SIM you want to set with napter.
+
+# example usage
+For test
+```bash
+$ ansible -i ./dynamic_inventory.sh all -m ping
+```
+
+Use playbook
+```bash
+$ ansible-playbook -i ./dynamic_inventory.sh playbook.yaml -vvv
+```
---
+++ dynamic_inventory.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SORACOM_CLI_PROFILE="XXXXX"
+TARGET_TAG_KEY="XXXXX"
+TARGET_TAG_VALUE="XXXXX"
+
+subscriberList=`soracom subscribers list --profile $SORACOM_CLI_PROFILE | jq '[.[] | select(.tags?.'$TARGET_TAG_KEY'=="'$TARGET_TAG_VALUE'") | .imsi ']`
+
+len=`echo ${subscriberList} | jq length`
+for i in `seq 0 $(expr ${len} - 1)`
+do
+    subscriber=`echo $subscriberList | jq -r .[${i}]`
+    existe_port_mapping=`soracom port-mappings get --imsi $subscriber --profile nao | jq .`
+
+    len=`echo ${existe_port_mapping} | jq length`;
+    if [ $len -eq 0 ]; then
+        new_port_mapping=`soracom port-mappings create --body '{"destination": {"imsi": "'$subscriber'", "port": 22}}' --profile nao | jq .`
+        arr+=`echo $new_port_mapping | jq .`
+        continue
+    fi
+
+    arr+=`echo $existe_port_mapping | jq .[]`
+
+done
+
+l=`echo $arr | jq -s -c flatten`
+echo $l | jq '. | map({ (.imsi) : { "hosts": [.ipAddress], "vars": {"ansible_port": .port, "ansible_user": "pi", "ansible_ssh_pass": "raspberry"}} })' | jq add
