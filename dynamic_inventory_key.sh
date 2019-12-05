d7c567e8a2a04fe833840c583d9dcafbc0d12cbc
naoyamaguchi <naoyamaguchi0128@gmail.com>
2019-12-05 20:17:11 +0900

    get ansible_user from api

--- dynamic_inventory_key.sh
+++ dynamic_inventory_key.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-SORACOM_CLI_PROFILE="XXXXX"
-TARGET_TAG_KEY="XXXXX"
-TARGET_TAG_VALUE="XXXXX"
-TARGET_USERNAME="XXXXX"
+# SORACOM_CLI_PROFILE="XXXXX"
+# TARGET_TAG_KEY="XXXXX"
+# TARGET_TAG_VALUE="XXXXX"
 
-subscriberList=`soracom subscribers list --fetch-all --profile $SORACOM_CLI_PROFILE | jq '[.[] | select(.tags?."'$TARGET_TAG_KEY'"=="'$TARGET_TAG_VALUE'") | .imsi ']`
+SORACOM_CLI_PROFILE="nao"
+TARGET_TAG_KEY="name"
+TARGET_TAG_VALUE="nao-device"
+
+subscriberList=`soracom subscribers list --fetch-all --profile "$SORACOM_CLI_PROFILE" | jq '[.[] | select(.tags?."'$TARGET_TAG_KEY'"=="'$TARGET_TAG_VALUE'") | { imsi: .imsi, tags: .tags? } ']`
 
 len=`echo ${subscriberList} | jq length`
 for i in `seq 0 $(expr ${len} - 1)`
 do
-    subscriber=`echo $subscriberList | jq -r .[${i}]`
-    existing_port_mapping=`soracom port-mappings get --imsi $subscriber --profile $SORACOM_CLI_PROFILE | jq .`
+    imsi=`echo $subscriberList | jq -r .[${i}].imsi`
+    ansible_user=`echo $subscriberList | jq .[${i}].tags.ansible_user`
 
-    len=`echo ${existing_port_mapping} | jq length`;
-    if [ $len -eq 0 ]; then
-        new_port_mapping=`soracom port-mappings create --body '{"destination": {"imsi": "'$subscriber'", "port": 22}}' --profile $SORACOM_CLI_PROFILE | jq .`
+    existing_port_mapping=`soracom port-mappings get --imsi $imsi --profile $SORACOM_CLI_PROFILE | jq '.[0] | { port_mapping: ., ansible_user: '$ansible_user' }'`
+
+    if [ -n `echo $existing_port_mapping | jq .port_mapping` ]; then
+        new_port_mapping=`soracom port-mappings create --body '{"destination": {"imsi": "'$imsi'", "port": 22}}' --profile "$SORACOM_CLI_PROFILE" | jq '. | { port_mapping: ., ansible_user: '$ansible_user' }'`
         arr+=`echo $new_port_mapping | jq .`
         continue
     fi
 
-    arr+=`echo $existing_port_mapping | jq .[]`
-
+    arr+=`echo $existing_port_mapping | jq .`
 done
 
 l=`echo $arr | jq -s -c flatten`
-echo $l | jq '. | map({ (.imsi) : { "hosts": [.ipAddress], "vars": {"ansible_port": .port, "ansible_user": "'$TARGET_USERNAME'"}} })' | jq add
+
+echo $l | jq '. | map({ (.port_mapping.imsi) : { "hosts": [.port_mapping.ipAddress], "vars": {"ansible_port": .port_mapping.port, "ansible_user": .ansible_user}} })' | jq add
+
+
